fb1f648de67f7e2b604500d6548930c75e0a2701
joshua horgen and kira roberts <joshua@>
2019-06-04 16:39:35 -0700

    jQuery Experimentation

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $( "button:first" ).click(function() {
+  $( "p:first" ).fadeToggle( "slow", "linear" );
+});
+$( "button:last" ).click(function() {
+  $( "p:last" ).fadeToggle( "fast", function() {
+    $( "#log" ).append( "<div>finished</div>" );
+  });
+
+}
